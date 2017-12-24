fcc49f94cc5b21798f140270e64c43f6e913f7c0
matt roll <mroll@arcwebtech.com>
2017-12-24 00:37:28 -0500

    adding a license to some code I copied
    modified:   emacs/.emacs.d/custom/bh-org.el

--- emacs/.emacs.d/custom/bh-org.el
+++ emacs/.emacs.d/custom/bh-org.el
@@ -1,3 +1,21 @@
+;; Copyright (C)  2013  Bernt Hansen.
+;; Permission is granted to copy, distribute and/or modify this document
+;; under the terms of the GNU Free Documentation License, Version 1.3
+;; or any later version published by the Free Software Foundation;
+;; with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.
+;;   
+;; Code in this document is free software: you can redistribute it
+;; and/or modify it under the terms of the GNU General Public
+;; License as published by the Free Software Foundation, either
+;; version 3 of the License, or (at your option) any later version.
+;;   
+;; This code is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+
+
 (defun bh/clock-in-task-by-id (id)
   "Clock in a task by id"
   (org-with-point-at (org-id-find id 'marker)
