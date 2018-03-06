876cde352bb2a36932ac9ef16371bbd0711867fd
matt roll <mroll@arcwebtech.com>
2018-03-06 14:55:33 -0500

    installing some new packages; setting some new keybindings to work w/ those packages
    modified:   news/.newsrc
    
    modified:   spacemacs/.emacsconfig/keys.el
    - keybindings for frame commands
    
    new file:   spacemacs/.emacsconfig/lib/frame-cmds/frame-cmds.el
    - library for manipulating frame dimensions and location
    
    modified:   spacemacs/.emacsconfig/pkgconfig/bh.el
    - comment out bad agenda line formatter
    
    modified:   spacemacs/.spacemacs
    - spotify layer
    - ruby layer
    - load frame-cmds

--- spacemacs/.emacsconfig/pkgconfig/bh.el
+++ spacemacs/.emacsconfig/pkgconfig/bh.el
@@ -991,9 +991,9 @@ so change the default 'F' binding in the agenda to allow both"
 (setq org-agenda-start-on-weekday 1)
 
 ;; Enable display of the time grid so we can see the marker for the current time
-(setq org-agenda-time-grid (quote ((daily today remove-match)
-                                   #("----------------" 0 16 (org-heading t))
-                                   (0900 1100 1300 1500 1700))))
+; (setq org-agenda-time-grid (quote ((daily today remove-match)
+;                                    #("----------------" 0 16 (org-heading t))
+;                                    (0900 1100 1300 1500 1700))))
 
 ;; Display tags farther right
 (setq org-agenda-tags-column -102)
